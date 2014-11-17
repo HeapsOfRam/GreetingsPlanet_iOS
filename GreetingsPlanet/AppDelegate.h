59dae82c6bc1b9b98053a48dfe4ae8a76cefc05f
Ryan March <rmarch03@villanova.edu>
2014-11-17 14:17:46 -0500

    Initial Commit

---
+++ GreetingsPlanet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreetingsPlanet
+//
+//  Created by Ryan March on 11/17/14.
+//  Copyright (c) 2014 Ryan March. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreetingsPlanet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreetingsPlanet
+//
+//  Created by Ryan March on 11/17/14.
+//  Copyright (c) 2014 Ryan March. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
