c040ae63ba18da238bc7ee31e15fa7dbc9d66b4f
Ryan March <rmarch03@villanova.edu>
2014-11-17 16:46:26 -0500

    Initial Faux Functionality Complete

--- GreetingsPlanet/ViewController.h
+++ GreetingsPlanet/ViewController.h
@@ -10,6 +10,19 @@
 
 @interface ViewController : UIViewController
 
+@property float red_value, green_value, blue_value;
+@property (weak, nonatomic) IBOutlet UILabel *warp_speed_text;
+@property (weak, nonatomic) IBOutlet UILabel *initiated_status;
+@property (weak, nonatomic) IBOutlet UILabel *upper_kirby;
+@property (weak, nonatomic) IBOutlet UILabel *lower_kirby;
+@property (weak, nonatomic) IBOutlet UIView *colors_panel;
+
+- (IBAction)initiate:(id)sender;
+- (IBAction)change_speed:(UISlider *)sender;
+- (IBAction)dance_switch:(UISwitch *)sender;
+- (IBAction)slide_rgb:(UISlider *)sender;
+
+
 
 @end
 
--- GreetingsPlanet/ViewController.m
+++ GreetingsPlanet/ViewController.m
@@ -14,14 +14,59 @@
 
 @implementation ViewController
 
+@synthesize red_value, blue_value, green_value, warp_speed_text, initiated_status, upper_kirby, lower_kirby, colors_panel;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)dance_switch:(UISwitch *)sender {
+    if([sender isOn]){
+        upper_kirby.text = @"<(\"<)(>\")>";
+        lower_kirby.text = @"(>\")><(\"<)";
+    }
+    else{
+        lower_kirby.text = @"<(\"<)(>\")>";
+        upper_kirby.text = @"(>\")><(\"<)";
+    }
+}
+
+- (IBAction)slide_rgb:(UISlider *)sender {
+    NSLog(@"Slider with TAG # %i clicked", (int)[sender tag]);
+    
+    if([sender tag] == 1){
+        red_value = sender.value;
+    }
+    if([sender tag] == 2){
+        green_value = sender.value;
+    }
+    if([sender tag] == 3){
+        blue_value = sender.value;
+    }
+    
+    self.view.backgroundColor = [UIColor colorWithRed:red_value green: green_value blue:blue_value alpha:1.0];
+}
+
+-(IBAction)initiate:(id)sender{
+    NSLog(@"Initated");
+    initiated_status.text = @"Initiated";
+    
+    colors_panel.hidden = (!colors_panel.hidden);
+}
+
+
+- (IBAction)change_speed:(UISlider *)sender {
+    warp_speed_text.text = [NSString stringWithFormat:@"Warp Speed = %.2f", sender.value];
+    
+    NSLog(@"Slider's value = %f", sender.value);
+}
+
 
 @end
